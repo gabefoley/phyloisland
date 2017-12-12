231dd48561ebd8fd4b93a9e956092f741a833655
gabefoley <gabriel.foley@uqconnect.edu.au>
2017-12-13 09:27:20 +1000

    Updated phyloinit.py and servers.py to default settings

--- phyloinit.py
+++ phyloinit.py
@@ -1,5 +1,5 @@
 from BioSQL import BioSeqDatabase
 
-server = BioSeqDatabase.open_database(driver="MySQLdb", user="pi", passwd="", host="localhost", db="newbase")
+server = BioSeqDatabase.open_database(driver="MySQLdb", user="pi", passwd="", host="localhost", db="phyloisland")
 db = server.new_database("newsmall", description="Just for testing")
 server.commit()
