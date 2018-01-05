df1e64121a1ba49f4b33024629d142c5bfece1bd
gabefoley <gabriel.foley@uqconnect.edu.au>
2018-01-06 02:53:57 +1000

    Merge - changing name of experimental to master

--- phyloinit.py
+++ phyloinit.py
@@ -1,5 +1,5 @@
 from BioSQL import BioSeqDatabase
 
-server = BioSeqDatabase.open_database(driver="MySQLdb", user="pi", passwd="", host="localhost", db="phyloisland")
+server = BioSeqDatabase.open_database(driver="MySQLdb", user="pi", passwd="", host="localhost", db="primates")
 db = server.new_database("newsmall", description="Just for testing")
 server.commit()
