6c90ee93a607a7397f9c930a50469172aa8549ee
Merodoc <rowan@overworld.net.au>
2018-06-21 23:15:18 +1000

    TODO - ToxinGraphicsMain - Add rest of features - default colors

--- ToxinGraphicsMain.py
+++ ToxinGraphicsMain.py
@@ -22,19 +22,26 @@ def writeImageToFile(ids):
     output_path = "tmp/"+name+".png"
     for record in query.all():
         
+        """ Create a dictionary for key = feature type -> value = location """
         locs = {}
+        # Prepare for literally the worst code in existence
+        """ We have to pull the features from location data in database
+        instead of directly from database because of current limitations """
         if getattr(record, "a1_loc") != "":
             locs["a1"] = getattr(record, "a1_loc").split(":")
         if getattr(record, "a2_loc") != "":
-            locs["a2"] = getattr(record, "a2_loc").split(":")       
+            locs["a2"] = getattr(record, "a2_loc").split(":")
+            
         
         
         print("adding %s genome to diagram" % (record.name))
         seq_record = servers.bio_db.lookup(primary_id = record.name)
+        
+        """ Add the features from dictionary to the seq_record features """
         for location in locs:
             feature = SeqFeature(location = FeatureLocation(int(locs[location][0]), int(locs[location][1]), strand=1), type = location)
             seq_record.features.append(feature)
-        
+        """ Set up the Genome Diagram """
         max_len = max(max_len, len(seq_record))
         gd_track_for_features = gd_diagram.new_track(1, name = 
         seq_record.name, greytrack = True, start = 0, end = len(seq_record))
@@ -48,7 +55,7 @@ def writeImageToFile(ids):
                                = str(i + 1), label_position = "start", label_size = 6, label_angle = 0)
             i+=1
 
-    # Draw and Write the Diagram to file
+    """ Draw and Write the Diagram to file """
     gd_diagram.draw(format="linear", pagesize = "A4", fragments = 0, start = 0, end = max_len)
     gd_diagram.write(output_path, "PNG")
     print("Genome Diagram has been added to file %s ", output_path)
