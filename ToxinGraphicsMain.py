2ac8878d249f853a830a1207eb1c7125bd0006a5
gabefoley <gabriel.foley@uqconnect.edu.au>
2018-09-15 19:40:43 +1000

    Added dynamic tracks for HMM images. Added overlap and max track parameters. Added correction for position of features on backwards strand

--- ToxinGraphicsMain.py
+++ ToxinGraphicsMain.py
@@ -148,105 +148,153 @@ def writeSeqToFile(ids):
         out_path = "tmp/"+ name + ".gb"
         SeqIO.write(seq_record, out_path, "genbank")
 
+
+
 def writeHMMToImage(hmm_dict, reference, region, seq_record, species):
-    # Currently taking region to be run simultaneously with hmmer, will need to manipulate some stuff for later
-    # Initiation of GenomeDiagram assets"
-    name = reference + "_" + species + "_GenomeDiagram"
-    gd_diagram = GenomeDiagram.Diagram(name)
-    max_len = 0
-    output_path = reference + "/" + species + ".png"
-    start = 0
-    # For my work I was considering changing 'region1, 2, and 3' to a3, TcB, and TcC for convenience
-    # Up to others though if I fully change that (is just a UI thing tbh)
-    region_colours = {"a1":"orange", "a2":"red", "chitinase":"green", "a3":"yellow",
-                      "TcB":"blue", "TcC":"magenta", "pore":"grey", "region1":"lightblue", "region2":"pink", "region3":"purple", "region4":"black"}
-    locs = {}
-    strandd = 1
-    for result in hmm_dict:
+        # Currently taking region to be run simultaneously with hmmer, will need to manipulate some stuff for later
+        # Initiation of GenomeDiagram assets"
+
+        # Convert the length of the genome into the length of the translated genome.
+        genome_length = round(len(seq_record) / 3)
+
+        #Parameters that can be set
+
+        # The highest number of tracks we'll attempt to add (if a feature overlaps on all tracks it'll just be added to the bottom track)
+        # Set max_tracks to None to just add as many as we need to get no overlap
+        max_tracks = None
+
+        # The amount we want to allow a feature to overlap with a neighbouring feature
+        overlap_amount = 0
+
+
+        print("In hmm code")
+        name = reference + "_" + species + "_GenomeDiagram"
+        gd_diagram = GenomeDiagram.Diagram(name)
+        max_len = 0
+        output_path = reference + "/" + species + ".png"
+        start = 0
+        # For my work I was considering changing 'region1, 2, and 3' to a3, TcB, and TcC for convenience
+        # Up to others though if I fully change that (is just a UI thing tbh)
+        region_colours = {"a1": "orange", "a2": "red", "chitinase": "green", "a3": "yellow",
+                          "TcB": "blue", "TcC": "magenta", "pore": "grey", "region1": "lightblue", "region2": "pink",
+                          "region3": "purple", "region4": "black"}
+        locs = {}
+        strandd = 1
+        for result in hmm_dict:
+            i = 0
+            for reg in result:
+                """ Create a dictionary for key = feature type -> value = location """
+                if "forward" in reg:
+                    strandd = 1
+                    locs[reg.split("/")[2] + phyloisland.randstring(5)] = result[reg].split(":")
+
+                # If it's on the backwards strand we need to correct the postion - swap the start and end positions and set them relative to the 5' end
+                # TODO: Actually this should be done in resultread, when we first read it in
+                elif "backward" in reg:
+                    strandd = -1
+                    positions = result[reg].split(":")
+                    print( "positions before split", positions)
+                    positions[0], positions[1] = (len(seq_record) - int(positions[1])) , (len(seq_record) - int(positions[0]))
+                    locs[reg.split("/")[2] + phyloisland.randstring(5)] = positions
+                    print( "positions after split", positions)
+
+
+                i += 1
+                # Prepare for literally the worst code in existence
+                """ We have to pull the features from location data in database
+                instead of directly from database because of current limitations """
+                # Brute force if statements to pull from database
+                # TODO - Could likely turn this into a for loop in so
+
+        """ Add the features from dictionary to the seq_record features """
+        feature_locations = []
+        # If want to add all features to image comment below line out
+        seq_record.features = []
         i = 0
-        for reg in result:       
-            """ Create a dictionary for key = feature type -> value = location """
-            if "forward" in reg:
-                strandd = 1
-            elif "backward" in reg:
-                strandd = -1
-            locs[reg.split("/")[2] + phyloisland.randstring(5)] = result[reg].split(":")
-            i += 1
-            # Prepare for literally the worst code in existence
-            """ We have to pull the features from location data in database
-            instead of directly from database because of current limitations """
-            # Brute force if statements to pull from database
-            # TODO - Could likely turn this into a for loop in so
-            
-    """ Add the features from dictionary to the seq_record features """
-    feature_locations = []     
-    # If want to add all features to image comment below line out
-    seq_record.features = []
-    i = 0
-    for location in locs:
-        """ Extract start and end values from each location, and add to independent lists """
-        """ create and add features based on locations """
-        feature = SeqFeature(location = FeatureLocation(int(locs[location][0]), int(locs[location][1]), strand=strandd), type = location[0:-5])
-        seq_record.features.append(feature)
+        for location in locs:
+            """ Extract start and end values from each location, and add to independent lists """
+            """ create and add features based on locations """
+            feature = SeqFeature(
+                location=FeatureLocation(int(locs[location][0]), int(locs[location][1]), strand=strandd),
+                type=location[0:-5])
+            seq_record.features.append(feature)
 
-    """ Set up the Genome Diagram """
-    max_len = max(max_len, len(seq_record))
+        """ Set up the Genome Diagram """
+        max_len = max(max_len, len(seq_record))
 
-    gd_track1 = gd_diagram.new_track(1, name = seq_record.name + " Track 1", greytrack = True, start = 0, end = len(seq_record))
-    gd_track2 = gd_diagram.new_track(2, name = seq_record.name + " Track 2", greytrack = True, start = 0, end = len(seq_record))
-    gd_feature_set1 = gd_track1.new_set()
-    gd_feature_set2 = gd_track2.new_set()
+        gd_track1 = gd_diagram.new_track(0, name=seq_record.name + " Track 1", greytrack=True, start=0,
+                                         end=len(seq_record))
+        gd_feature_set1 = gd_track1.new_set()
 
-    for feature in seq_record.features:
-        feature_locations.append((feature.location.start, feature.location.end))
+        for feature in seq_record.features:
+            feature_locations.append((feature.location.start, feature.location.end))
 
-    for feature in seq_record.features:
-        if i == 0:
-            gd_feature_set1.add_feature(seq_record.features[i], label = True, name = seq_record.features[i].type, color = region_colours[seq_record.features[i].type],
-                                        label_position = "middle")
-            while i +1 < len(feature_locations):
-                overlap = feature.location.start in feature_locations[i + 1] or feature.location.end in feature_locations[i+ 1]
-                i +=1
+        print("This many features")
+        print(len(seq_record.features))
+
+        total_tracks = 1
+        # Dictionary to keep track of which locations are at which track
+        forward_tracks = {1 : []}
+        backward_tracks = {1 : []}
+
+        for feature in seq_record.features:
+            feature_added = False
+            current_track = 1
+
+            while current_track <= total_tracks and not feature_added:
+
+                current_strand = feature.location.strand
+
+                if current_strand == 1:
+                    dict_track = forward_tracks
+                elif current_strand == -1:
+                    dict_track = backward_tracks
+
+
+                overlap = False
+                for loc in dict_track[current_track]:
+                    if feature.location.start - overlap_amount in loc or feature.location.end + overlap_amount in loc:
+                        overlap = True
+
+
                 if overlap:
-                    gd_feature_set2.add_feature(seq_record.features[i], label = True, name = seq_record.features[i].type, color = region_colours[seq_record.features[i].type],
-                                        label_position = "middle")
-                else:
-                    gd_feature_set1.add_feature(seq_record.features[i], label = True, name = seq_record.features[i].type, color = region_colours[seq_record.features[i].type],
-                                        label_position = "middle")
-    # Add Features
-    """ 
-    for feature in seq_record.features:
-        if feature.type in region_colours.keys():
-            #check if location is in any existing locations
-            try:
-                for features in gd_feature_set1:
-                    if feature.location == features.location:
-                        break
-                    elif features.location.start in feature.location or features.location.end in feature.location:
-                        gd_feature_set2.add_feature(feature, label = True, name
-                                                    = feature.type, color = region_colours[feature.type], label_position = "start", label_size = 6, label_angle = 0)
+
+                    # If we've reached the highest track, make a new track
+                    if current_track == total_tracks:
+                        if max_tracks is not None and current_track == max_tracks:
+                                # We've reached the highest track we want to try and add, so just add it to the bottom track (there will be overlap)
+                                gd_feature_set1.add_feature(feature, label=True, name=feature.type, color=region_colours[feature.type], label_position='middle')
+                                dict_track[1].append(feature.location)
+                                feature_added = True
+                        else:
+                            # Add to this track
+                            current_track += 1
+                            total_tracks += 1
+                            dict_track[current_track] = []
+                            exec("gd_track" + str(current_track) + "= gd_diagram.new_track(0, name=seq_record.name + ' Track " + str(current_track) + "', greytrack=True, start=0, end=len(seq_record))")
+                            exec("gd_feature_set" + str(current_track) + " = gd_track" + str(current_track) + ".new_set()")
+
                     else:
-                        gd_feature_set1.add_feature(feature, label = True, name = feature.type, color = region_colours[feature.type], label_position = "start", label_size =6, label_angle = 0)
-            except:
-                gd_feature_set1.add_feature(feature, label = True, name = feature.type, color = region_colours[feature.type], label_position = "start", label_size =6, label_angle = 0)
-                """
-                 
-    """    
-    For 'Zoomed' sections:     
-    start = max(start, min(svals))
-    if start > 1500:
-        start -= 1000
-        end = min(len(seq_record), max(endvals))
-     if len(seq_record) - end > 1500:
-         end += 1000
-         """
-            
-    """ Draw and Write the Diagram to file """
-    gd_track1.add_set(gd_feature_set1)
-    gd_track2.add_set(gd_feature_set2)
-    gd_diagram.draw(format="linear", pagesize = "A2", fragments = 0, start = start, end = len(seq_record))
-    gd_diagram.write(output_path, "PNG")
-    print("Genome Diagram has been added to file " + output_path)
+                        # Check on higher track
+                        current_track += 1
+
+                else:
+                    # Add to this track
+                    exec("gd_feature_set" + str(current_track) + ".add_feature(feature, label=True, name=feature.type, color=region_colours[feature.type], label_position='middle')")
+                    if current_track in dict_track:
+                        dict_track[current_track].append(feature.location)
+                    else:
+                        dict_track[current_track] = [feature.location]
+                    feature_added = True
+
+
+        for track in range(1, total_tracks):
+            exec("gd_track"+ str(track) + ".add_set(gd_feature_set" + str(track) +")")
+        gd_diagram.draw(format="linear", pagesize="A2", fragments=0, start=start, end=len(seq_record))
+        gd_diagram.write(output_path, "PNG")
+        print("Genome Diagram has been added to file " + output_path)
+
+
     
 def writeHmmToSeq(hmm_dict, reference, region, seqrecord, species):
     name = species + "_sequence"
